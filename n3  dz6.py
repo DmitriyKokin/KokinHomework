9e1cdcc1e0231db2180a5be8521fe317a71ebf66
DmitriyKokin <60880186+DmitriyKokin@users.noreply.github.com>
2021-12-10 17:14:37 +0300

    Update n3  dz6.py

--- n3  dz6.py
+++ n3  dz6.py
@@ -10,18 +10,17 @@ try:
 except:
     print(f'file not found')
     exit()
-
 a.close()
 print(f'Текст, прочитанный из файла:\n{mytext}\n')
 try:
-    key = int(input(f'Введите ключ шифрования..(int) '))
+    key = int(input(f'Введите ключ шифрования..(int)'))
 except:
     print(f'Unknown Error')
     exit()
 e = encrypt(mytext, key)
 out = open('output3.txt', 'w')
 out.write(f'{e}')
-print(f'encrypt(x2):\n{encrypt(e,key)}')
+print(f'encrypt(x2):\n{encrypt(e, key)}')
 out.close()
 
 
