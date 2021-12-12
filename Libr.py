d36a06d43af35128ba92c10c2fc286905f4327a5
DmitriyKokin <60880186+DmitriyKokin@users.noreply.github.com>
2021-12-12 15:20:15 +0300

    Домашняя работа за 7 занятие

--- Libr.py
+++ Libr.py
@@ -1,11 +1,7 @@
-
 def printer():
     print(f'function works successfully')
-
 def fprint(str):
     print(f'{str}')
-    
-
 if __name__ == '__main__':
     print('one two one two')
     for i in range(1000):
